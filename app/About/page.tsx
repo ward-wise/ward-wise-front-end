35685785b4fc47bfb2a77865cde60e5b216f6e39
Sean MacMullan <sean.macmullan@gmail.com>
2024-01-20 14:50:14 -0600

    Fix typo. Rename folder. Put "Ward Wise" on temp home page.

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -6,7 +6,7 @@ export default function About() {
     <div className="max-w-2xl mx-auto p-6">
       <h1 className="text-3xl font-bold mb-6">About</h1>
       <p className="mb-8">
-        Word Wise is a breakout group under Chi Hack Night. Our source code is available on GitHub. We use data scraped from the Chicago Capital Improvements Archive by Jake Smith and John C. Ruf.
+        Ward Wise is a breakout group under Chi Hack Night. Our source code is available on GitHub. We use data scraped from the Chicago Capital Improvements Archive by Jake Smith and John C. Ruf.
       </p>
       <Link href="https://github.com/chihacknight/breakout-groups/issues/224" target="_blank" rel="noopener noreferrer" className="text-cyan-500 underline">
         Check out our Chi Hack Night project page
