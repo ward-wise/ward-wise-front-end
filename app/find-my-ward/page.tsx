ac4d4d8e5e3d11aa270ee044eb7eb4b18f17fff2
Sean MacMullan <sean.macmullan@gmail.com>
2024-02-27 22:12:58 -0600

    Add WardSearch component to find-my-ward page for testing purposes.

--- app/find-my-ward/page.tsx
+++ app/find-my-ward/page.tsx
@@ -1,8 +1,22 @@
-export default function FindMyWard() {
+import WardSearch from "../components/ui/WardSearch";
+
+export default function FindMyWard({
+  searchParams,
+}: {
+  searchParams?: {
+    ward?: string
+  };
+}) {
+
+  const ward = searchParams?.ward;
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>This is the Find My Ward page</h1>
+        <WardSearch />
+        {ward &&
+          <p>Ward {ward}</p>
+        }
       </div>
     </main>
   );
