a9aaf4feeab09afd8876f3cb29e7f60df04fc8a3
Sean MacMullan <sean.macmullan@gmail.com>
2024-03-10 15:28:10 -0500

    Add spending menu page text and adjust card layout styling.

--- app/(pages)/alderman-spending-menu/page.tsx
+++ app/(pages)/alderman-spending-menu/page.tsx
@@ -8,9 +8,22 @@ export const metadata: Metadata = {
 export default function SpendingMenu() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>This is the Alderman Spending Menu page</h1>
-        <MenuItemCard itemName="Alley Speed Hump Program" unitCost={1400} unit="block" isVisionZero={true} imgPath="/images/menu-items/AlleySpeedHumpProgram.png"/>
+      <div className="flex flex-col z-10 max-w-5xl w-full">
+        <h1 className="text-2xl font-bold mt-2 mb-4">
+          Aldermanic Spending Menu
+        </h1>
+        <p>
+          Each year, CDOT and OBM provide alders a list of standard menu items. Costs are estimated based on previous years&apos; costs. Alders select items from this list to allocate their $1.5 million budget.
+        </p>
+        <p className='text-center my-6 mt-10 italic'>
+          Select an item to learn more.
+        </p>
+        <div className="grid grid-cols-1 xl:grid-cols-2 gap-x-6 gap-y-6">
+          <MenuItemCard itemName="Alley Speed Hump Program" unitCost={1400} unit="block" isVisionZero={true} imgPath="/images/menu-items/AlleySpeedHumpProgram.png" />
+          <MenuItemCard itemName="Alley Speed Hump Program" unitCost={1400} unit="block" isVisionZero={true} imgPath="/images/menu-items/AlleySpeedHumpProgram.png" />
+          <MenuItemCard itemName="Alley Speed Hump Program" unitCost={1400} unit="block" isVisionZero={true} imgPath="/images/menu-items/AlleySpeedHumpProgram.png" />
+          <MenuItemCard itemName="Alley Speed Hump Program" unitCost={1400} unit="block" isVisionZero={true} imgPath="/images/menu-items/AlleySpeedHumpProgram.png" />
+        </div>
       </div>
     </main>
   );
