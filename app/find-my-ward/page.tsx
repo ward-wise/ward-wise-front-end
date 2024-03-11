ec5ad02054b421015f287450cc27d4ea54b83a17
Noah Appelbaum <noahappelbaum@gmail.com>
2024-03-10 20:45:25 -0500

    functioning page with contact card on ward lookup

--- app/find-my-ward/page.tsx
+++ app/find-my-ward/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import WardSearch from "../components/ui/WardSearch";
 import WardSpendingButton from "../components/ui/WardSpendingButton";
+import AlderContactCard from "../components/ui/AlderContactCard";
 
 export const metadata: Metadata = {
   title: 'Find My Ward',
@@ -21,9 +22,8 @@ export default function FindMyWard({
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <WardSearch />
-        {ward &&
-          <WardSpendingButton />
-        }
+        {ward && <AlderContactCard wardNumber={+ward} />}
+        {ward && <WardSpendingButton />}
       </div>
     </main>
   );
