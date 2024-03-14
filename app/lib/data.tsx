69a6795fad71fa64ecc9c9756585dd8c3c2e8b28
Noah Appelbaum <noahappelbaum@gmail.com>
2024-03-14 18:18:43 -0500

    cleaned up syntax

--- app/lib/data.tsx
+++ app/lib/data.tsx
@@ -9,7 +9,7 @@ const prisma = new PrismaClient();
 export async function getWardContactInfo(ward:number):Promise<WardContactInfo> {
     const wardContactInfo = await prisma.ward_contact_info.findUnique({
         where: {
-            ward: ward,
+            ward,
         },
     })
     return wardContactInfo;
