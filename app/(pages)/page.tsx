711681ddd4255c18be4356f3537acb3df99d9342
smckay1031 <smckay099@gmail.com>
2024-03-30 17:34:29 -0500

    removed find-my-ward section

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,3 +1,6 @@
+import WardSearch from "../components/ui/WardSearch";
+import FindMyWard from "./find-my-ward/page";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
@@ -6,17 +9,13 @@ export default function Home() {
         <span className="font-medium text-xl leading-5 pl-2"> Keeping the government accountable</span>
       </h1>
       </section>
-        <article className="flex items-center justify-center my-5 py-10">
+        <article className="flex items-center justify-center my-5 py-10" id="WhatIs">
           <h2 className=" text-center font-bold text-lg w-6/12 leading-5">What Is A Ward?</h2>
-          <p className=" flex-wrap w-7/12 text-sm leading-4 tracking-tight indent-3">The City of Chicago is divided into fifty legislative districts or wards. Each district is represented by an alderperson who is elected by their constituency to serve a four year term.</p>
+          <p className="text-left flex-wrap w-7/12 text-sm leading-4 tracking-tight indent-3">The City of Chicago is divided into fifty legislative districts or wards. Each district is represented by an alderperson who is elected by their constituency to serve a four year term.</p>
         </article>
-        <section className="flex items-center justify-center my-5 py-10">
-          <p className="text-lg w-5/12"> $1.5 Million</p>
+        <section className="flex items-center justify-center my-5 py-10" id="HowMuch">
+          <p className="text-lg w-5/12 text-center"> $1.5 Million</p>
           <h2 className=" text-center font-bold text-lg w-7/12 leading-5">How Much Funding Does My Ward Get?</h2>
-        </section>
-        <section id="findWard">
-          <h3>Find My Ward?</h3>
-
         </section>
     </main>
   );
