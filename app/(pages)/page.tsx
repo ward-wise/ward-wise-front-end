8bb02ebab06cf99e84d782f2dbd7672b11323133
smckay1031 <smckay099@gmail.com>
2024-03-30 14:55:50 -0500

    first text of hero section layout (mobile only)

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Welcome to Ward-wise! This is the home page.</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div className="flex items-center justify-center flex-col w-screen min-h-80 p-10 bg-gray-100" id="hero">
+      <h1 className="font-bold text-5xl flex flex-col justify-around m-1">Ward Wise
+        <span className="font-medium text-xl leading-5 pl-2"> Keeping the government accountable</span>
+      </h1>
+      </div>
     </main>
   );
 }
