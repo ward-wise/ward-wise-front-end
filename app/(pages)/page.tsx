855b6f9ad68fe060e739002a3f05737f2137015d
smckay1031 <smckay099@gmail.com>
2024-03-31 14:54:37 -0500

    added cards to text/article sections

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -9,11 +9,11 @@ export default function Home() {
       <Hero />
       <article className="flex items-center justify-center my-5 p-5 gap-4 mt-80" id="WhatIs">
         <h2 className=" h-full text-center lg:text-center font-bold text-lg md:text-2xl lg:text-4xl 3xl:text-5xl w-5/12 leading-5">What Is A Ward?</h2>
-        <p className=" p-4 text-left flex-wrap w-6/12 text-sm lg:text-xl 2xl:text-2xl lg:leading-6 leading-4 tracking-tight border-l-4 lg:border-l-8 border-sky-400">The City of Chicago is divided into fifty legislative districts called wards. Each ward is represented by an alderperson who is elected by their constituency to manage their ward's budget and perform other civic duties.</p>
+        <p className=" p-4 text-left flex-wrap w-6/12 text-sm lg:text-xl 2xl:text-2xl lg:leading-6 leading-4 tracking-tight border-l-4 lg:border-l-8 border-sky-400 shadow-xl rounded-lg">The City of Chicago is divided into fifty legislative districts called wards. Each ward is represented by an alderperson who is elected by their constituency to manage their ward's budget and perform other civic duties.</p>
       </article>
         <section className="flex items-center justify-center my-5 py-10 gap-4" id="HowMuch">
-          <p className="text-xl lg:text-4xl 2xl:text-5xl w-6/12 text-center"> $1.5 Million</p>
-          <h2 className=" p-2 text-center w-7/12 font-bold text-lg md:text-xl lg:text-4xl 3xl:text-5xl leading-5 border-l-4 lg:border-l-8 border-sky-400">How Much Funding Does My Ward Get?</h2>
+          <p className="p-5 text-xl lg:text-4xl 2xl:text-5xl w-6/12 text-center shadow-xl rounded-lg border-r-4 lg:border-r-8 border-sky-400"> $1.5 Million</p>
+          <h2 className=" p-2 text-center w-7/12 font-bold text-lg md:text-xl lg:text-4xl 3xl:text-5xl leading-5">How Much Funding Does My Ward Get?</h2>
         </section>
     </main>
   );
