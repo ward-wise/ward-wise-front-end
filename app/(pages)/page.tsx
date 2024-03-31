aa45e215762e19c1ca5e81123d56579303a85476
smckay1031 <smckay099@gmail.com>
2024-03-30 21:08:57 -0500

    altered ward description

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       </section>
         <article className="flex items-center justify-center my-5 p-5 gap-4" id="WhatIs">
           <h2 className=" h-full text-center lg:text-center text-sky-400 font-bold text-lg md:text-2xl lg:text-4xl 3xl:text-5xl w-5/12 leading-5">What Is A Ward?</h2>
-          <p className=" p-2 text-left flex-wrap w-6/12 text-sm lg:text-xl 2xl:text-2xl lg:leading-6 leading-4 tracking-tight border-l-4 lg:border-l-8 border-sky-400">The City of Chicago is divided into fifty legislative districts or wards. Each district is represented by an alderperson who is elected by their constituency to serve a four year term.</p>
+          <p className=" p-2 text-left flex-wrap w-6/12 text-sm lg:text-xl 2xl:text-2xl lg:leading-6 leading-4 tracking-tight border-l-4 lg:border-l-8 border-sky-400">The City of Chicago is divided into fifty legislative districts called wards. Each ward is represented by an alderperson who is elected by their constituency to manage their ward's budget and perform other civic duties.</p>
         </article>
         <section className="flex items-center justify-center my-5 py-10 gap-4" id="HowMuch">
           <p className="text-xl lg:text-4xl 2xl:text-5xl w-6/12 text-center"> $1.5 Million</p>
