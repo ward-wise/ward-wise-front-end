05135b183685e2599a9841e14d0a4f586898bfcc
smckay1031 <smckay099@gmail.com>
2024-03-31 14:48:55 -0500

    chnaged sub-title text color

--- app/components/ui/Hero.tsx
+++ app/components/ui/Hero.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Hero() {
     return(
         <section className="flex items-center justify-center flex-col w-full h-72 absolute top-20 bg-slate-800" id="hero">
-            <h1 className="font-semibold text-4xl lg:text-7xl text-white flex flex-col justify-around">Ward Wise
-            <span className="font-medium text-lg lg:text-4xl text-white leading-5 pl-4"> Keeping the government accountable</span>
+            <h1 className="font-medium text-4xl lg:text-7xl text-white flex flex-col justify-around">Ward Wise
+            <span className="font-medium text-lg lg:text-3xl text-sky-500 leading-5 pl-2"> Keeping The Government Accountable</span>
             </h1>
       </section>
     );
