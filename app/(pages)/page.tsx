4444118dd692b124fa63bf0e88c3fa36f9c96f8f
smckay1031 <smckay099@gmail.com>
2024-03-31 20:31:02 -0500

    changed color of hero subtitle and fixed margins of text cards

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -5,15 +5,15 @@ import Hero from "../components/ui/Hero";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-around">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <Hero />
-      <article className=" w-full flex items-center justify-center mt-80 shadow-xl rounded-lg border-l-2 lg:border-l-4 " id="WhatIs">
+      <article className=" w-full flex items-center justify-center mt-96 shadow-xl rounded-lg border-l-2 lg:border-l-4 " id="WhatIs">
         <h2 className=" p-5 h-60 text-center flex justify-center items-center font-bold text-lg md:text-2xl lg:text-4xl 3xl:text-5xl w-2/4 leading-5">What Is A Ward?</h2>
         <p className="h-60 p-4 flex justify-center items-center text-left w-2/4 text-sm lg:text-xl 2xl:text-2xl lg:leading-6 leading-4 tracking-tight border-l-4 lg:border-l-8 border-sky-400 rounded-lg">The City of Chicago is divided into fifty legislative districts called wards. Each ward is represented by an alderperson who is elected by their constituency to manage their ward's budget and perform other civic duties.</p>
       </article>
-        <section className="my-32 w-full flex items-center justify-center shadow-xl rounded-lg border-l-2 lg:border-l-4" id="HowMuch">
-          <p className="p-8 h-36 w-2/4 text-xl lg:text-4xl 2xl:text-5xl flex justify-center items-center text-center border-r-4 lg:border-r-8 border-sky-400 rounded-lg"> $1.5 Million</p>
-          <h2 className="p-8 h-36 w-2/4 flex items-center justify-center font-bold text-lg md:text-xl lg:text-4xl 3xl:text-5xl leading-5">How Much Funding Does My Ward Get?</h2>
+        <section className="w-full  my-24 md:my-48  flex items-center justify-center shadow-xl rounded-lg border-l-2 lg:border-l-4" id="HowMuch">
+          <p className="p-8 h-60 w-2/4 text-xl lg:text-4xl 2xl:text-5xl flex justify-center items-center text-center border-r-4 lg:border-r-8 border-sky-400 rounded-lg"> $1.5 Million</p>
+          <h2 className="p-8 h-60 w-2/4 flex items-center justify-center font-bold text-lg md:text-xl lg:text-4xl 3xl:text-5xl leading-5">How Much Funding Does My Ward Get?</h2>
         </section>
     </main>
   );
