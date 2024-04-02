40b0a8ca3b79d84bd2644704ae714f076bc9c62c
Sean MacMullan <sean.macmullan@gmail.com>
2024-04-01 20:31:41 -0500

    Update text on home page.

--- app/components/ui/Hero.tsx
+++ app/components/ui/Hero.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 export default function Hero() {
-    return(
+    return (
         <section className="flex items-center justify-center flex-col w-full h-72 absolute top-20 bg-slate-800" id="hero">
-            <h1 className="font-medium text-4xl lg:text-7xl text-white flex flex-col justify-around">Ward Wise
-            <span className="font-medium text-lg lg:text-3xl text-sky-400 leading-5 pl-2"> Keeping The Government Accountable</span>
-            </h1>
-      </section>
+            <div className="px-8">
+                <h1 className="font-medium text-4xl lg:text-7xl text-white mb-2">Ward Wise</h1>
+                <h2 className="font-medium text-lg lg:text-3xl text-sky-400 leading-5 lg:pl-2">Aldermanic Spending on Neighborhood Infrastructure</h2>
+            </div>
+
+        </section>
     );
 }
