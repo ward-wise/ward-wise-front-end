ccf85efa0f41cabd73ee9618a28d1353b90c809d
Sean MacMullan <sean.macmullan@gmail.com>
2024-04-07 15:25:28 -0500

    Add placeholder leaflet map to maps page.

---
+++ app/(pages)/maps/Map.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { MapContainer, TileLayer, Marker, useMapEvents, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import "leaflet-defaulticon-compatibility";
+
+const Map = () => {
+
+    return (
+        <MapContainer
+            center={[51.505, -0.09]}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{ height: "50vh" }}
+        >
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[51.505, -0.09]}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer>
+    );
+};
+
+export default Map;
--- app/(pages)/maps/page.tsx
+++ app/(pages)/maps/page.tsx
@@ -1,14 +1,19 @@
-import { Metadata } from "next";
-
+import { Metadata } from "next"
+import dynamic from 'next/dynamic';
 export const metadata: Metadata = {
   title: 'Maps',
 };
 
+const MapWithNoSSR = dynamic(() => import("./Map"), {
+  ssr: false,
+});
+
 export default function Maps() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
         <h1>This is the Maps page</h1>
+        <MapWithNoSSR/>
       </div>
     </main>
   );
