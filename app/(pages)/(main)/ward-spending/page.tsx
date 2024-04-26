742f9a8c90d4deb7b5972ce4fa68932ce09965d6
Sean MacMullan <sean.macmullan@gmail.com>
2024-04-25 21:43:45 -0500

    Move data demo from console log to html. Add search params implementation.

--- app/(pages)/(main)/ward-spending/page.tsx
+++ app/(pages)/(main)/ward-spending/page.tsx
@@ -6,18 +6,44 @@ export const metadata: Metadata = {
   title: 'Ward Spending',
 };
 
-export default async function WardSpending() {
+export default async function WardSpending({
+  searchParams,
+}: {
+  searchParams?: { ward?: string; year?: string};
+}) {
 
   // FIXME: THIS IS A DEMO THAT THIS QUERY IS WORKING
-  const data = await getWardSpendingItems(47, 2022);
-  console.log("Got data!", data);
+  let ward = searchParams?.ward ? +searchParams?.ward : 1;
+  let year = searchParams?.year ? +searchParams?.year : 2023;
+  const data = await getWardSpendingItems(ward, year);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center lg:flex">
-        <WardSelect/>
-        <YearSelect/>
+        <WardSelect />
+        <YearSelect />
       </div>
+      {data &&
+        <DataDemo data={data} />
+      }
     </main>
   );
 }
+
+
+const DataDemo = ({ data }: { data: any }) => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      {data.map((item: any, index: number) => (
+        <div key={index} className="bg-gray-100 rounded-lg shadow-md p-4 mb-4">
+          <h2 className="text-xl font-semibold mb-2">{item.item}</h2>
+          <p className="text-gray-700 mb-2">Category: {item.category}</p>
+          <p className="text-gray-700 mb-2">Ward: {item.ward}</p>
+          <p className="text-gray-700 mb-2">Year: {item.year}</p>
+          <p className="text-gray-700 mb-2">Location: {item.location}</p>
+          <p className="text-gray-700 mb-2">Cost: {item.cost}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
