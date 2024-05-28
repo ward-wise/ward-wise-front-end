d750669ca0016349e80af0518af625a3b2809b3b
Noah Appelbaum <noahappelbaum@gmail.com>
2024-05-28 13:53:06 -0500

    adjusted for responsive display

--- app/(pages)/(main)/find-my-ward/page.tsx
+++ app/(pages)/(main)/find-my-ward/page.tsx
@@ -17,8 +17,8 @@ export default function FindMyWard({
   const ward = searchParams?.ward;
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-inter text-sm lg:flex">
+    <main className="flex min-h-screen flex-col items-center justify-between lg:p-x-24">
+      <div className="z-10 flex flex-col gap-y-2 max-w-5xl w-full font-inter text-sm">
         <WardSearch />
         {/* Display Contact Card and Spending Button for valid wards */}
         {ward && +ward > 0 && +ward <= 50 ? (
