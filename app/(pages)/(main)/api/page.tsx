601fff85a4275cf064bcc10de599c982be22bfd8
Noah Appelbaum <noahappelbaum@gmail.com>
2024-06-11 14:49:44 -0500

    componentized styling for endpoint descriptions

--- app/(pages)/(main)/api/page.tsx
+++ app/(pages)/(main)/api/page.tsx
@@ -14,61 +14,98 @@ export default function Page () {
         {/* Endpoint Descriptions */}
         <div className="flex flex-col gap-8">
           {/* Ward Contact Info */}
-          <div>
-            <h2 className="font-bold">/wardcontactinfo/[ward]</h2>
-
-            <p>
-              Get contact and web presence info for alders and ward offices.
-              Access information for all wards at{" "}
-              <span className="italic">/wardcontactinfo</span>
-            </p>
-            <span className="bg-green-400 rounded-sm px-1 text-white">GET</span>
-            <p className="inline ml-2 bg-slate-600 rounded-sm text-white px-4 font-mono">
-              wardwisechicago.org/api/ward
-            </p>
-            <p className="mt-4">
-              Or, get information for a specific ward at{" "}
-              <span className="italic">.../ward</span>.
-            </p>
-            <span className="bg-green-400 rounded-sm px-1 text-white">GET</span>
-            <p className="inline ml-2 bg-slate-600 rounded-sm text-white px-4 font-mono">
-              wardwisechicago.org/api/ward/47
-            </p>
-          </div>
+          <Endpoint
+            title="/wardcontactinfo/[ward]"
+            id="wardcontactinfo"
+            entries={[
+              {
+                description: (
+                  <p>
+                    Get contact and web presence info for alders and ward
+                    offices. Access information for all wards at{" "}
+                    <span className="italic">/wardcontactinfo</span>
+                  </p>
+                ),
+                method: "GET",
+                url: "wardwisechicago.org/api/ward",
+              },
+              {
+                description: (
+                  <p>
+                    Or, get information for a specific ward at{" "}
+                    <span className="italic">.../ward</span>.
+                  </p>
+                ),
+                method: "GET",
+                url: "wardwisechicago.org/api/ward/47",
+              },
+            ]}
+          />
 
           {/* Spending Items */}
-          <div>
-            <h2 className="font-bold">/spendingitems</h2>
-
-            <p>
-              Get information about aldermanic menu expenditures from 2005 to
-              the present day.
-            </p>
-            <span className="bg-green-400 rounded-sm px-1 text-white">GET</span>
-            <p className="inline ml-2 bg-slate-600 rounded-sm text-white px-4 font-mono">
-              wardwisechicago.org/api/spendingitems
-            </p>
-            <p className="mt-4">
-              You can filter spending item results with query strings, to get
-              results by <span className="italic">ward</span>,{" "}
-              <span className="italic">year</span>, and/or{" "}
-              <span className="italic">category</span>.
-            </p>
-            <span className="bg-green-400 rounded-sm px-1 text-white">GET</span>
-            <p className="inline ml-2 bg-slate-600 rounded-sm text-white px-4 font-mono">
-              wardwisechicago.org/api/spendingitems?ward=12&year=2023&category=Street%20Resurfacing
-            </p>
-          </div>
+          <Endpoint
+            title="/spendingitems"
+            id="spendingitems"
+            entries={[
+              {
+                description: (
+                  <p>
+                    Get information about aldermanic menu expenditures from 2005
+                    to the present day.
+                  </p>
+                ),
+                method: "GET",
+                url: "wardwisechicago.org/api/spendingitems",
+              },
+              {
+                description: (
+                  <p>
+                    You can filter spending item results with query strings, to
+                    get results by <span className="italic">ward</span>,{" "}
+                    <span className="italic">year</span>, and/or{" "}
+                    <span className="italic">category</span>.
+                  </p>
+                ),
+                method: "GET",
+                url: "wardwisechicago.org/api/spendingitems?ward=12&year=2023&category=Street%20Resurfacing",
+              },
+            ]}
+          />
         </div>
 
         <p className="text-center text-sm md:w-1/2 md:mx-auto">
           Having trouble getting the information you want? Have an idea for
           another way that we could use or provide access to our data? Drop us a
           line on{" "}
-          <a className="font-semibold" target="_blank" rel="noopener noreferrer" href="https://github.com/ward-wise/ward-wise-front-end">GitHub.</a>{" "}
+          <a
+            className="font-semibold"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/ward-wise/ward-wise-front-end"
+          >
+            GitHub.
+          </a>{" "}
           Having a great time using our data for something interesting? Let us
           know that, too!
         </p>
       </div>
     );
+}
+
+function Endpoint({ title, id, entries }: { title: string, id?: string, entries: {description: JSX.Element; method: string; url: string}[] }) {
+  return (
+    <div id={id}>
+      <h2 className="font-bold">{title}</h2>
+      {entries.map((e, i) =>
+        <>
+          <div className={i > 0 ? "mt-4" : ""}>
+            {e.description}
+          </div>
+          <span className="bg-green-400 rounded-sm px-1 text-white">{e.method}</span>
+          <p className="inline ml-2 bg-slate-600 rounded-sm text-white px-4 font-mono"> {e.url}
+          </p>
+        </>
+      )}
+    </div>
+  );
 }
